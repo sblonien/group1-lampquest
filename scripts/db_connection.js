f38f5a6fdd4d986ec1ab542a8b0402f93f302186
Sawal Maskey <sawal_maskey1@baylor.edu>
2018-10-29 23:24:44 +0000

    first commit

--- scripts/db_connection.js
+++ scripts/db_connection.js
@@ -1,10 +1,6 @@
 var mysql = require('mysql');
-var credentials = require('../../db_credential/config');
+var credentials = require('../../credentials/config');
 
 var pool = mysql.createPool(credentials.prod);
-
-
-
-
 
 module.exports = { connection_pool: pool };
